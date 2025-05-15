67f60aadfa05e71aff6ffdb93d28ae02054cb0cc
Youngpuffy <levimuturi2@gmail.com>
2025-05-15 15:14:39 +0300

    class attributes

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,31 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count ={}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist =artist
+        self.genre = genre
+
+        Song.count += 1
+
+        Song.genres.append(genre)
+
+        Song.artists.append(artist)
+
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+
+        else:
+            Song.genre_count[genre] = 1
+
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+
+        else:
+            Song.artist_count[artist] = 1
+
+    
